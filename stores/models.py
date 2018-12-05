1a81f2f82e1405e020d604cd9f849ac45d3d23b9
ismail <ismailrzouki7@gmail.com>
2018-12-05 21:13:14 +0000

    Add some extra fields on Store model (category and vicinity and also icon)

--- stores/models.py
+++ stores/models.py
@@ -6,10 +6,13 @@ import datetime
 class Store(models.Model):
     name = models.CharField(max_length=120)
     distance = models.IntegerField(default=0)
-    users = models.ManyToManyField(User, related_name='stores')
+    users = models.ManyToManyField(User, related_name='stores', null=True)
     is_preffered = models.BooleanField(default=False)
     is_blocked = models.BooleanField(default=False)
     date = models.DateTimeField(default=datetime.datetime.now)
-    
+    category = models.CharField(default='Not Specified' ,max_length=120)
+    vicinity = models.CharField(default='Not Specified', max_length=120)
+    icon = models.CharField(default='Not Specified', max_length=400)
+
     def __str__(self):
-        return '{0} store away by {1} meters'.format(self.name, self.distance, self.is_preffered, self.date)
+        return '{0} store away by {1} meters, under category {2}'.format(self.name, self.distance, self.category)
--- stores/views.py
+++ stores/views.py
@@ -11,14 +11,13 @@ import pytz
 import geocoder, requests
 
 def index(request):
+    ''' This is home page view that contains all nearby shops '''
     if request.user.is_authenticated:
         current_user = User.objects.get(pk=request.user.id)
         # get all stores tha are blocked 
         blocked_stores = current_user.stores.filter(is_blocked=True)
         for bstore in blocked_stores:
-            print("date :",bstore.date)
             hours = abs(datetime.now(tz=pytz.utc) - bstore.date).total_seconds() / 3600.0
-            print('hs :', hours)
             # if any of the store are passed the 2h time being blocked unblock them
             if hours >= 2:
                 bstore.is_blocked = False
@@ -33,6 +32,7 @@ def index(request):
         return render(request, 'stores/login.html')
 
 def login_v(request):
+    ''' Log the user in if everything is fine '''
     if request.method == 'POST':
         username = request.POST.get('username', False) # was a problem
         password = request.POST.get('password', False)
@@ -65,25 +65,29 @@ def register(request):
         return render(request, 'stores/register.html', {'form' : form})
 
 def log_out(request):
+    ''' Log Out the current user '''
     logout(request)
     return HttpResponseRedirect(reverse('login'))
 
 def get_data(request):
+    ''' Get longtitde and latitude from ser ip address and then get all nearby stores by sending an HTTP GET request to the url below'''
     latitude, longtitude =  tuple(map(str, geocoder.ip('me').latlng))
     r = requests.get('https://places.cit.api.here.com/places/v1/browse?at=35.7226%2C-5.9357&q=shopping&size=200&Accept-Language=en-US%2Cen%3Bq%3D0.9&app_id=3iwRXjOK1iYH4KPMUYdC&app_code=4u-MGNNl4aGp1t_0r-3_tg')
     rj = r.json()
     results = rj['results']['items']
     for result in results:
-        s = Store.objects.create(name=result['title'], distance=result['distance'])
+        s = Store.objects.create(name=result['title'], distance=result['distance'], category=result['category']['title'], vicinity=result['vicinity'], icon=result['icon'])
         s.users.add(User.objects.get(pk=request.user.id))
     
 def add_preffer(request, store_id):
+    ''' Add store to prefer list '''
     store = Store.objects.get(pk=store_id)
     store.is_preffered = True
     store.save()
     return HttpResponseRedirect(reverse('favorite'))
 
 def favorites(request):
+    ''' preferred list view to display all preferred shops '''
     current_user = User.objects.get(pk=request.user.id)
     pstores = current_user.stores.filter(is_preffered=True)
     paginator = Paginator(pstores, 10)
@@ -92,12 +96,14 @@ def favorites(request):
     return render(request, 'stores/prefferedPage.html', {'pstores' : stores})
 
 def remove_from_favorites(request, store_id):
+    ''' Remove store from favorite list '''
     store = Store.objects.get(pk=store_id)
     store.is_preffered = False
     store.save()
     return HttpResponseRedirect(reverse('favorite'))
 
 def dislike_store(request, store_id):
+    ''' Dislike a Store '''
     store = Store.objects.get(pk=store_id)
     store.is_blocked = True
     store.date = datetime.now(tz=pytz.utc)
