fc01e6282f02d990cba616d579356c2d2f70b062
ismail <ismailrzouki7@gmail.com>
2018-12-05 11:11:30 +0000

    Add two fields for checking if the Store is disabled and also a time Field so when someone disable some Store instance i would like to turn on isDisabled field and also save the current time (modify the already existing time) so i can after compare dates

--- stores/models.py
+++ stores/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 
 class Store(models.Model):
@@ -7,6 +8,8 @@ class Store(models.Model):
     distance = models.IntegerField(default=0)
     users = models.ManyToManyField(User, related_name='stores')
     is_preffered = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(default=False)
+    date = models.DateTimeField(default=datetime.datetime.now)
 
     def __str__(self):
-        return '{0} - {1} store by {2} meters , isPreffered : {3}'.format(self.id, self.name, self.distance, self.is_preffered)
+        return '{0} - {1} store by {2} meters , isPreffered : {3}, time : {4}'.format(self.id, self.name, self.distance, self.is_preffered, self.date)
